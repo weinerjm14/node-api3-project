4befb7b047f417582a0941a33e14390e8728ae74
Jennifer Weiner <58009503+weinerjm14@users.noreply.github.com>
2020-06-11 17:16:00 -0400

    logger added

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// code away!
+const server = require('./server');
+
+const port = 5000;
+
+server.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 
 const server = express();
+server.use(express.json());
+server.use(logger);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
@@ -8,6 +10,14 @@ server.get('/', (req, res) => {
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log(
+    `[${new Date().toISOString()}] ${req.method} to ${req.url} from ${req.get(
+      'Origin'
+    )}`
+  );
+
+  next();
+}
 
 module.exports = server;
